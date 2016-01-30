2c3f6057751ea2546f12b85ca88bbf6082f05dc8
Denis <denis.pob@mail.ru>
2016-01-30 11:27:49 +0200

    1

--- warehouse-database/src/main/resources/migration/V1__item_table.sql
+++ warehouse-database/src/main/resources/migration/V1__item_table.sql
@@ -1,4 +1,8 @@
 CREATE TABLE warehouse.item (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50)
+  name VARCHAR(50),
+  description VARCHAR(250),
+  longitude FLOAT,
+  latitude FLOAT,
+  capacity INT
 )
