d8b397feff7018fbe09949cdae304a2f60d6a68b
Denis <denis.pob@mail.ru>
2016-02-26 21:01:57 +0200

    Sessionfactory issue was fixed

--- warehouse/src/main/java/com/dao/WarehouseDao.java
+++ warehouse/src/main/java/com/dao/WarehouseDao.java
@@ -3,7 +3,9 @@ package com.dao;
 import java.util.List;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.entities.Warehouse;
@@ -11,6 +13,9 @@ import com.entities.Warehouse;
 @Component
 public class WarehouseDao extends BaseDao {
 
+	@Autowired
+	private SessionFactory sessionFactory;
+	
 	public void save(Warehouse p) {
 		Session session = sessionFactory.openSession();
 		Transaction tx = session.beginTransaction();
