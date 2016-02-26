40c031e963e91c156a773f4e5c0ea237e9eb71d7
Sergey Goncharov <sgon@KH083.softservecom.com>
2016-02-26 17:28:34 +0200

    fixes

--- warehouse/src/main/java/com/dao/BaseDao.java
+++ warehouse/src/main/java/com/dao/BaseDao.java
@@ -1,10 +1,6 @@
 package com.dao;
 
-import java.util.List;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import com.entities.Warehouse;
 
 
 public abstract class BaseDao {
