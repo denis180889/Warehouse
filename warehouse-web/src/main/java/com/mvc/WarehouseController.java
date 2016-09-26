bb4b6a94a9d66269a45f2b93c7ca753fa8de29cb
dpobochniy <denis.pob@mmail.ru>
2016-09-26 15:41:00 +0300

    Spring Security

--- warehouse-web/src/main/java/com/mvc/WarehouseController.java
+++ warehouse-web/src/main/java/com/mvc/WarehouseController.java
@@ -3,12 +3,18 @@ package com.mvc;
 import com.dto.Warehouse;
 import com.service.WarehouseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collection;
 
 @Controller
 public class WarehouseController {
@@ -25,6 +31,17 @@ public class WarehouseController {
     public String showWarehouses(ModelMap model) {
         model.addAttribute("warehouses", warehouseService.getWarehouses());
         model.addAttribute("warehouse", new Warehouse());
+
+        Collection<GrantedAuthority> authorities = (Collection<GrantedAuthority>) SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        boolean hasRole = false;
+        for (GrantedAuthority authority : authorities) {
+            hasRole = authority.getAuthority().equals("ROLE_ADMIN");
+            if (hasRole) {
+                break;
+            }
+        }
+        model.addAttribute("admin", hasRole);
+
         return "result";
     }
 
--- warehouse-web/src/main/webapp/WEB-INF/jsp/result.jsp
+++ warehouse-web/src/main/webapp/WEB-INF/jsp/result.jsp
@@ -1,7 +1,6 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
 <html>
 <head>
     <title>Spring MVC Form Handling</title>
@@ -20,7 +19,11 @@
             </c:forEach>
 </table>
 
+<c:if test="${admin}">
+
 <jsp:include page="addWarehouse.jsp"></jsp:include>
+
+</c:if>
 
 </body>
 </html>
